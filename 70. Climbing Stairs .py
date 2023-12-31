d40ad7168a23a9e9c62be8ed4c9679a0ecb8d6b2
Yifan Zhang <133056456+ChristineZhang0126@users.noreply.github.com>
2023-12-31 12:06:26 -0700

    Add files via upload

---
+++ 70. Climbing Stairs .py
@@ -0,0 +1,43 @@
+# 70. Climbing Stairs
+# Easy
+# 20.7K
+# 720
+# Companies
+# You are climbing a staircase. It takes n steps to reach the top.
+
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+# Example 1:
+
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+# Example 2:
+
+# Input: n = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0] * n
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2,n):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n-1]
+

c9b5c1ccc671fef28e7857377ddde8002647a728
Yifan Zhang <133056456+ChristineZhang0126@users.noreply.github.com>
2023-12-31 12:40:47 -0700

    Add files via upload

---
+++ 153. Find Minimum in Rotated Sorted Array .py
@@ -0,0 +1,55 @@
+# 153. Find Minimum in Rotated Sorted Array
+# Medium
+# 12.5K
+# 549
+# Companies
+# Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+# [4,5,6,7,0,1,2] if it was rotated 4 times.
+# [0,1,2,4,5,6,7] if it was rotated 7 times.
+# Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+# Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+# You must write an algorithm that runs in O(log n) time.
+
+ 
+
+# Example 1:
+
+# Input: nums = [3,4,5,1,2]
+# Output: 1
+# Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+# Example 2:
+
+# Input: nums = [4,5,6,7,0,1,2]
+# Output: 0
+# Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+# Example 3:
+
+# Input: nums = [11,13,15,17]
+# Output: 11
+# Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
+
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            # If mid element is greater than the last element, search in the right half
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            # If mid element is less than or equal to the last element, search in the left half
+            else:
+                right = mid
+
+        # At this point, left and right converge to the minimum element
+        return nums[left]
+
+        
